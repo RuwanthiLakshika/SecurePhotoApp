dc97d51c7ece43dfa2e31d194ecbd045840e4dcb
Ruwanthi Lakshika <125971277+RuwanthiLakshika@users.noreply.github.com>
2025-04-24 19:40:23 +0530

    initial changes

---
+++ SecurePhotoApp/SecurePhotoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SecurePhotoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
