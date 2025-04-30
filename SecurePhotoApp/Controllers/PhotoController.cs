50b8ab6fb9ec44ee319245f9f46a5038f3a1246c
Ruwanthi Lakshika <125971277+RuwanthiLakshika@users.noreply.github.com>
2025-04-30 16:34:58 +0530

    changes related to photo gallery

--- SecurePhotoApp/Controllers/PhotoController.cs
+++ SecurePhotoApp/Controllers/PhotoController.cs
@@ -2,6 +2,7 @@
 using SecurePhotoApp.Models;
 using Azure.Storage.Blobs;
 using Azure.Identity;
+using Azure.Storage.Blobs.Models;
 
 namespace SecurePhotoApp.Controllers
 {
@@ -65,5 +66,77 @@ namespace SecurePhotoApp.Controllers
                 return fileName; 
             }
         }
+
+        public async Task<IActionResult> Gallery(string searchTerm = "", string sortOrder = "newest")
+        {
+            try
+            {
+                var username = User.Identity.Name ?? "user";
+                var serviceUri = new Uri($"https://{storageAccountName}.blob.core.windows.net");
+
+                BlobServiceClient blobServiceClient = new BlobServiceClient(serviceUri, new DefaultAzureCredential());
+
+                BlobContainerClient container = blobServiceClient.GetBlobContainerClient(containerName);
+
+                var blobItems = new List<BlobItem>();
+                await foreach (var blobItem in container.GetBlobsAsync())
+                {
+                    blobItems.Add(blobItem);
+                }
+
+                var userPhotos = blobItems.Where(b => b.Name.StartsWith(username));
+
+                if (!string.IsNullOrEmpty(searchTerm))
+                {
+                    userPhotos = userPhotos.Where(b => b.Name.Contains(searchTerm, StringComparison.OrdinalIgnoreCase));
+                }
+
+                switch (sortOrder.ToLower())
+                {
+                    case "oldest":
+                        userPhotos = userPhotos.OrderBy(b => b.Properties.CreatedOn);
+                        break;
+                    case "name":
+                        userPhotos = userPhotos.OrderBy(b => b.Name);
+                        break;
+                    case "size":
+                        userPhotos = userPhotos.OrderByDescending(b => b.Properties.ContentLength);
+                        break;
+                    case "newest":
+                    default:
+                        userPhotos = userPhotos.OrderByDescending(b => b.Properties.CreatedOn);
+                        break;
+                }
+
+                var photos = new List<PhotoItemVM>();
+                foreach (var blobItem in userPhotos)
+                {
+                    var blobClient = container.GetBlobClient(blobItem.Name);
+
+                    photos.Add(new PhotoItemVM
+                    {
+                        Name = blobItem.Name,
+                        Url = blobClient.Uri.AbsoluteUri,
+                        UploadDate = blobItem.Properties.CreatedOn?.DateTime ?? DateTime.MinValue,
+                        Size = blobItem.Properties.ContentLength ?? 0
+                    });
+                }
+
+                var model = new GalleryVM
+                {
+                    Photos = photos,
+                    SearchTerm = searchTerm,
+                    SortOrder = sortOrder
+                };
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
+        }
+
     }
 }
+    
--- SecurePhotoApp/Models/PhotoVM.cs
+++ SecurePhotoApp/Models/PhotoVM.cs
@@ -7,4 +7,63 @@ namespace SecurePhotoApp.Models
 
     }
 
+    public class PhotoItemVM
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public DateTime UploadDate { get; set; }
+        public long Size { get; set; }
+
+        // Helper properties
+        public string FileName => System.IO.Path.GetFileName(Name);
+        public string FormattedSize
+        {
+            get
+            {
+                if (Size < 1024)
+                    return $"{Size} B";
+                else if (Size < 1024 * 1024)
+                    return $"{Size / 1024:F1} KB";
+                else
+                    return $"{Size / (1024 * 1024):F1} MB";
+            }
+        }
+
+        public string FormattedDate => UploadDate.ToString("MMMM dd, yyyy");
+        public string TimeAgo
+        {
+            get
+            {
+                var span = DateTime.Now - UploadDate;
+
+                if (span.TotalDays > 365)
+                    return $"{(int)(span.TotalDays / 365)} year{((int)(span.TotalDays / 365) == 1 ? "" : "s")} ago";
+                if (span.TotalDays > 30)
+                    return $"{(int)(span.TotalDays / 30)} month{((int)(span.TotalDays / 30) == 1 ? "" : "s")} ago";
+                if (span.TotalDays > 1)
+                    return $"{(int)span.TotalDays} day{((int)span.TotalDays == 1 ? "" : "s")} ago";
+                if (span.TotalHours > 1)
+                    return $"{(int)span.TotalHours} hour{((int)span.TotalHours == 1 ? "" : "s")} ago";
+                if (span.TotalMinutes > 1)
+                    return $"{(int)span.TotalMinutes} minute{((int)span.TotalMinutes == 1 ? "" : "s")} ago";
+
+                return "Just now";
+            }
+        }
+    }
+
+    public class PhotoDetailVM : PhotoItemVM
+    {
+        public string ContentType { get; set; }
+    }
+
+    public class GalleryVM
+    {
+        public List<PhotoItemVM> Photos { get; set; }
+        public string SearchTerm { get; set; }
+        public string SortOrder { get; set; }
+    }
+
 }
+
+
